e536864ce1b59cf8d503a8699db1c8184d5eec3d
Jennifer C <yenifercastro09@gmail.com>
2024-04-21 02:02:36 -0600

    Traducciones

--- pothohub/src/components/errorpage.jsx
+++ pothohub/src/components/errorpage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const ErrorPage = (props) => {
   return (
     <div class="bg-gray-100 mt-24">
-        <h3 class="font-bold text-xl text-indigo-600">Error Page</h3>
+        <h3 class="font-bold text-xl text-indigo-600">Pagina no encontrada</h3>
     </div>
     );
 };
--- pothohub/src/components/herosection.jsx
+++ pothohub/src/components/herosection.jsx
@@ -9,9 +9,9 @@ export const HeroSections = (props) => {
         </div>
         
         <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-        <h1 class="text-7xl font-bold leading-tight mb-4">Bienvenido</h1>
-        <p class="text-2xl text-gray-300 mb-8">Descubre increibles imagenes.</p>
-        <a href="#" class="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">Vamos</a>
+        <h1 class="text-7xl font-bold leading-tight mb-4">Aventura Visual</h1>
+        <p class="text-2xl text-gray-300 mb-8">La fotografía es la literatura del ojo</p>
+        
         </div>
     </div>
     );
